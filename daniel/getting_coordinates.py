8b728915ed5b8c6b7a824699886b1a9640df489b
danielkahnsolorzano <97979267+danielkahnsolorzano@users.noreply.github.com>
2023-04-11 13:34:33 -0500

    Update getting_coordinates.py

--- daniel/getting_coordinates.py
+++ daniel/getting_coordinates.py
@@ -62,3 +62,131 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# coding: utf-8
+"""
+NatNet client https://github.com/mje-nz/python_natnet/blob/master/LICENSE
+"""
+
+"""
+import argparse
+import attr
+import natnet
+"""
+
+"""
+class ClientApp(object):
+    _client = attr.ib()
+    @classmethod
+    def connect(cls, server_name, quiet):
+        client = natnet.Client.connect(server_name)
+        if client is None:
+            return None
+        return cls(client, quiet)
+    def run(self):
+        self._client.set_callback(self.callback)
+        self._client.spin() # spin(timeout=None) Continuously receive and process messages
+    def callback(self, rigid_bodies, markers, timing):
+        if rigid_bodies:
+            for body in rigid_bodies: 
+                if body.id_==60:
+                    bodyPos = body.position
+                    print(f'x:{bodyPos[0]} y:{bodyPos[1]} z:{bodyPos[2]}')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', help='Will autodiscover if not supplied')
+    parser.add_argument('--rate', type=float, default=10, help='Rate at which to produce fake data (Hz)')
+    args = parser.parse_args()
+    try:
+        app = ClientApp.connect(args.server, args.rate, True)
+        app.run()
+    except natnet.DiscoveryError as e:
+        print('Error:', e)
+if __name__ == '__main__':
+    main()
+"""
+
+"""
+class ClientApp(object):
+    _client = attr.ib()
+    _retPos = attr.ib(default=None)
+    @classmethod
+    def connect(cls, server_name, quiet):
+        client = natnet.Client.connect(server_name)
+        if client is None:
+            return None
+        return cls(client, quiet)
+    def run(self):
+        self._client.set_callback(self.callback)
+        if self._retPos is not None:
+            print("RETURNING POSITION") #DEBUGGING
+            print(self._retPos) #DEBUGGING
+            return self._retPos
+        else:
+            print("TRYING AGAIN") #DEBUGGING
+            print(self._retPos) #DEBUGGING
+            self.run()
+    def callback(self, rigid_bodies, markers, timing):
+        if rigid_bodies:
+            for body in rigid_bodies: 
+                if body.id_==60:
+                    bodyPos = body.position
+                    self._retPos = bodyPos
+                    print(self._retPos) #DEBUGGING
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', help='Will autodiscover if not supplied')
+    parser.add_argument('--rate', type=float, default=10, help='Rate at which to produce fake data (Hz)')
+    args = parser.parse_args()
+    try:
+        app = ClientApp.connect(args.server, args.rate, True)
+        currPos = app.run()
+        print("GOT HERE") #DEBUGGING
+        print(currPos) #DEBUGGING
+    except natnet.DiscoveryError as e:
+        print('Error:', e)
+if __name__ == '__main__':
+    main()
+"""
+
+"""
+class ClientApp(object):
+    _client = attr.ib()
+    _retPos = attr.ib(default=None)
+    @classmethod
+    def connect(cls, server_name, quiet):
+        client = natnet.Client.connect(server_name)
+        if client is None:
+            return None
+        return cls(client, quiet)
+    def run(self):
+        if self.__class__._retPos is not None:
+            print("RETURNING POSITION") #DEBUGGING
+            print(self.__class__._retPos) #DEBUGGING
+            return self.__class__._retPos
+        else:
+            print("TRYING AGAIN") #DEBUGGING
+            print(self.__class__._retPos) #DEBUGGING
+            self.run()
+    def callback(self, rigid_bodies, markers, timing):
+        if rigid_bodies:
+            for body in rigid_bodies: 
+                if body.id_==60:
+                    bodyPos = body.position
+                    self.__class__._retPos = bodyPos #MAYBE THIS WILL WORK
+                    print(self.__class__._retPos) #DEBUGGING
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', help='Will autodiscover if not supplied')
+    parser.add_argument('--rate', type=float, default=10, help='Rate at which to produce fake data (Hz)')
+    args = parser.parse_args()
+    try:
+        app = ClientApp.connect(args.server, args.rate, True)
+        currPos = app.run()
+        print("GOT HERE") #DEBUGGING
+        print(currPos) #DEBUGGING
+    except natnet.DiscoveryError as e:
+        print('Error:', e)
+if __name__ == '__main__':
+    main()
+"""

bdcc6160d0254c6b8f89d91d138868e419b70ef9
danielkahnsolorzano <97979267+danielkahnsolorzano@users.noreply.github.com>
2023-04-11 19:41:15 -0500

    Add files via upload

---
+++ daniel/call_get_coord.py
@@ -0,0 +1,50 @@
+from getting_coordinates import ClientApp
+import argparse
+
+"""
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', help='Will autodiscover if not supplied')
+    parser.add_argument('--rate', type=float, default=10, help='Rate at which to produce fake data (Hz)')
+    parser.add_argument('--quiet', action='store_true')
+    args = parser.parse_args()
+    try:
+        app = ClientApp.connect(args.server, args.rate, args.quiet)
+        print(app.run())
+    except natnet.DiscoveryError as e:
+        print('Error:', e)
+if __name__ == '__main__':
+    main()
+"""
+
+"""
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', help='Will autodiscover if not supplied')
+    parser.add_argument('--rate', type=float, default=10, help='Rate at which to produce fake data (Hz)')
+    parser.add_argument('--quiet', action='store_true')
+    args = parser.parse_args()
+    try:
+        pos = ClientApp.connect(args.server, args.rate, args.quiet)
+        print(pos)
+    except natnet.DiscoveryError as e:
+        print('Error:', e)
+if __name__ == '__main__':
+    main()
+"""
+import natnet
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', help='Will autodiscover if not supplied')
+    parser.add_argument('--rate', type=float, default=10, help='Rate at which to produce fake data (Hz)')
+    parser.add_argument('--quiet', action='store_true')
+    args = parser.parse_args()
+    try:
+        ClientApp.connect(args.server, args.rate, args.quiet)
+        file = open('data.txt', 'r')
+        data = file.read()
+        print(data)
+    except natnet.DiscoveryError as e:
+        print('Error:', e)
+if __name__ == '__main__':
+    main()

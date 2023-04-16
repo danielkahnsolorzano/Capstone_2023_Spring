2a9956b1531afcd86cc65bed45c01d708a14b3b7
danielkahnsolorzano <97979267+danielkahnsolorzano@users.noreply.github.com>
2023-04-16 18:19:21 -0500

    Create coord.py

---
+++ daniel/coord.py
@@ -0,0 +1,17 @@
+"""
+Tryign something different.
+"""
+
+import pynatnet
+
+def callback(timestamp, rigid_bodies, markers, force_plates):
+    for body in rigid_bodies:
+        if body.id == 60:
+            position = body.position
+            print(f"Position: ({position[0]}, {position[1]}, {position[2]})")
+
+client = pynatnet.connect("127.0.0.1", "127.0.0.1")
+client.set_callback(callback)
+
+while True:
+    client.poll()
